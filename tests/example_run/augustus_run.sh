7652e8e95aaacb7a85c5feb395466e98dbdec725
Jackson <80480626+jacksonhturner@users.noreply.github.com>
2026-02-16 13:12:53 -0500

    Update augustus_run.sh
    making example input paths consistent

--- tests/example_run/augustus_run.sh
+++ tests/example_run/augustus_run.sh
@@ -1,5 +1,5 @@
-nextflow run main.nf \
-    --input ../test_orthogarden/metadata.csv \
+nextflow run /path/to/orthogarden/main.nf \
+    --input /path/to/metadata.csv \
     --publish_dir ../test_orthogarden/test_run1/ \
     --kraken_db /pickett_shared/databases/k2_pluspf_20230314/ \
     -profile local,eight \
